7d7ad6faffb477744e893f6d4a7a6011bd48fc81
Samhita Vetcha <52694492+vsamhita2028@users.noreply.github.com>
2021-08-14 03:42:20 +0530

    deployment-commit

--- Index.js
+++ Index.js
@@ -26,46 +26,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     }).catch((err) => console.log(err));
     
 app.use("/college",CollegeRouter);
-app.get("/",(req,res)=>{
-    //colleges by state : count
-
-    // College.aggregate([{$group: {_id:"$State",count: { $sum: 1 }}}])
-    // .then((result)=>{
-    //     console.log(result.length);
-    //     res.send(result);
-    // })
-
-    // colleges offering a particular course : count
-
-    // College.aggregate([{$unwind:"$Courses"},{$group: {_id:"$Courses",count: { $sum: 1 }}}])
-    // .then((result)=>{
-    //     console.log(result.length);
-    //     res.send(result);
-    // })
-    
-    // List of colleges offering a particular course :list
-    
-    // College.aggregate([{$unwind:"$Courses"},{$match:{Courses:"B.Tech Electrical and Electronics Engineering"}}])
-    // .then((result)=>{
-    //     console.log(result.length);
-    //     res.send(result);
-    // })
-
-    //colleges in that state  : list
-
-    // College.find({State:"Andhra Pradesh"})
-    // .then((result)=>{
-    //     console.log(result.length);
-    //     res.send(result);
-    // })
-})
-
-
-
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/one-stop/build")))
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "react-app", "build", "index.html"))
+        res.sendFile(path.join(__dirname, "one-stop", "build", "index.html"))
     })
 }
